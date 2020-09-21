90db9490efdcd52253bc6030ed5a787593f68399
snailrita <snailmailrita@gmail.com>
2020-09-21 07:24:57 +0100

    Revert "Revert "Added spalsh screen""
    This reverts commit c088d60c286272c768d4adc9b5767aea852a5d31.

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFistScene", 2f);
+    }
+
+    void LoadFistScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
